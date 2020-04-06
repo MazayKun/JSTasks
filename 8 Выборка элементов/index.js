39b70a6e1d85273f9a180066b903a32ac3c45d1d
MazayKun <kir99916@gmail.com>
2020-04-06 11:03:25 +0300

    Сделал что то вроде запросов к бд, только немного странное (функциональный код это не мое)

--- 8 Выборка элементов/index.js
+++ 8 Выборка элементов/index.js
@@ -4,14 +4,49 @@
  * @returns {Array}
  */
 function query(collection) {
+    var copy = collection.slice();
+    for (var i = 1; i < arguments.length; i++) {
+        if(arguments[i].name === 'filter') {
+            copy = _filterIn(copy, arguments[i].property, arguments[i].values);
+        }
+    }
+    for (var i = 1; i < arguments.length; i++) {
+        if(arguments[i].name === 'select') {
+            copy = _select(copy, arguments[i].property);
+        }
+    }
+    return copy;
+}
 
+function _select(collection, property) {
+    var answer = [];
+    for (var i = 0; i < collection.length; i++) {
+        var tmp = {}
+        for (var j = 0; j < property.length; j++) {
+            if(collection[i].hasOwnProperty(property[j]) ){
+                tmp[property[j]] = collection[i][property[j]];
+            }
+        }
+        answer.push(tmp);
+    }
+    return answer;
+}
+
+function _filterIn(collection, property, values) {
+    for(var i = 0; i < collection.length; i++) {
+        if(values.indexOf(collection[i][property]) === -1){
+            collection.splice(i, 1);
+            i--;
+        }
+    }
+    return collection;
 }
 
 /**
  * @params {String[]}
  */
 function select() {
-
+    return {name : 'select' , property : [].slice.call(arguments)};
 }
 
 /**
@@ -19,7 +54,7 @@ function select() {
  * @param {Array} values – Массив разрешённых значений
  */
 function filterIn(property, values) {
-
+    return {name : 'filter' , property : property , values : values};
 }
 
 module.exports = {
